a645c2f64e9e77f1edaf23aab67716bcc75edcfd
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-04-22 19:54:30 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -3,19 +3,26 @@ def call() {
     
    echo 'Installing AWS CLI...'
                     sh '''
-                        echo "Installing AWS CLI..."
-                        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-                        unzip -o awscliv2.zip
-                       ./aws/install --bin-dir $HOME/bin --install-dir $HOME/aws-cli --update
-                        rm -rf awscliv2.zip aws
+                         echo "Installing AWS CLI..."
+                         mkdir -p $HOME/bin
+                         curl -o awscliv2.zip "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+                         unzip -o awscliv2.zip
+                         ./aws/install --bin-dir $HOME/.local/bin --install-dir $HOME/.local/aws-cli --update
+                         rm -rf awscliv2.zip aws
+                         export PATH=$HOME/.local/bin:$PATH
+                         echo "PATH is set to: $PATH"
+                         echo "Validating AWS CLI installation..."
+                         $HOME/.local/bin/aws --version
                     '''    
                     sh '''
-                         echo "Installing Terraform..."
-                         curl -o terraform_1.5.0_linux_amd64.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
-                         unzip -o terraform_1.5.0_linux_amd64.zip -d $HOME/bin/
-                         rm terraform_1.5.0_linux_amd64.zip
-                         echo $PATH
-                         export PATH=$HOME/bin:$PATH
+                          echo "Installing Terraform..."
+                          curl -o terraform_1.5.0_linux_amd64.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
+                          unzip -o terraform_1.5.0_linux_amd64.zip -d $HOME/bin/
+                          rm terraform_1.5.0_linux_amd64.zip
+                          export PATH=$HOME/bin:$PATH
+                          echo "PATH is set to: $PATH"
+                          echo "Validating Terraform installation..."
+                          $HOME/bin/terraform version
                     '''
     
     echo "Terraform and AWS CLI installation complete!"
