7cd14f8e784418602b108647f3980e828c23bcd7
Akhil M <akhilm@Akhils-MacBook-Air.local>
2025-04-22 17:56:16 +0530

    test

---
+++ vars/test.groovy
@@ -0,0 +1,25 @@
+def call() {
+    echo "Installing Terraform and AWS CLI on Linux"
+    
+    sh '''
+        # Install AWS CLI
+        echo "Installing AWS CLI..."
+        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+        unzip -q awscliv2.zip
+        sudo ./aws/install
+        aws --version
+        
+        # Install Terraform
+        echo "Installing Terraform..."
+        TERRAFORM_VERSION="1.5.7"
+        wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+        unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+        sudo mv terraform /usr/local/bin/
+        terraform --version
+        
+        # Clean up
+        rm -rf aws awscliv2.zip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+    '''
+    
+    echo "Terraform and AWS CLI installation complete!"
+}
