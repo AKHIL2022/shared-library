0b32ab2bf49aece693e9d7fd4bfef4a773e5ac78
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-04-22 19:23:25 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -11,7 +11,7 @@ def call() {
                     '''    
                     sh '''
                          echo "Installing Terraform..."
-                         curl "https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip"
+                         curl -o terraform_1.5.0_linux_amd64.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
                          unzip -o terraform_1.5.0_linux_amd64.zip -d $HOME/bin/
                          rm terraform_1.5.0_linux_amd64.zip
                     '''
