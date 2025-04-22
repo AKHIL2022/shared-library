d450a04601f53ab653b7c6b86476d4d8adcd5a92
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-04-22 19:00:49 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,25 +1,29 @@
 def call() {
     echo "Installing Terraform and AWS CLI on Linux"
     
-    sh '''
-        # Install AWS CLI
-        echo "Installing AWS CLI..."
-        curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-        unzip -q awscliv2.zip
-        ./aws/install
-        aws --version
-        
-        # Install Terraform
-        echo "Installing Terraform..."
-        TERRAFORM_VERSION="1.5.7"
-        wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-        unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-        mv terraform /usr/local/bin/
-        terraform --version
-        
-        # Clean up
-        rm -rf aws awscliv2.zip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-    '''
+   echo 'Installing AWS CLI...'
+                    sh '''
+                        # Download and unzip AWS CLI
+                        curl -s -o awscliv2.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
+                        unzip -q awscliv2.zip
+                        
+                        # Install and verify
+                        ./aws/install --bin-dir /usr/local/bin --install-dir /usr/local/aws-cli --update
+                        aws --version
+                    '''
+                    
+                    // Install Terraform
+                    echo 'Installing Terraform...'
+                    sh '''
+                        TERRAFORM_VERSION="1.5.7"
+                        # Download and unzip Terraform
+                        wget -q https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+                        unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+                        
+                        # Install and verify
+                        mv terraform /usr/local/bin/
+                        terraform --version
+                    '''
     
     echo "Terraform and AWS CLI installation complete!"
 }
