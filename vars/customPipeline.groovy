6dcda94b68be6cd84bc1e05eb195b9c514fafcbd
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-04-28 09:39:53 +0530

    Update customPipeline.groovy

--- vars/customPipeline.groovy
+++ vars/customPipeline.groovy
@@ -25,18 +25,14 @@ def call(Map config) {
 
         stage('Install Terraform') {
             steps {
-                sh '''#!/bin/bash
-                    apt-get update && apt-get install -y \\
-                        gnupg \\
-                        software-properties-common \\
-                        lsb-release
-
-                    curl -fsSL https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-                    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-                    
-                    # Install Terraform
-                    apt-get update && apt-get install -y terraform
-                '''
+                 sh '''
+                          echo "Installing Terraform..."
+                          curl -o terraform_1.5.0_linux_amd64.zip https://releases.hashicorp.com/terraform/1.5.0/terraform_1.5.0_linux_amd64.zip
+                          unzip -o terraform_1.5.0_linux_amd64.zip -d $HOME/bin/
+                          rm terraform_1.5.0_linux_amd64.zip
+                          export PATH=$HOME/bin:$PATH
+                          echo "PATH is set to: $PATH"
+                       '''
             }
         }
 
