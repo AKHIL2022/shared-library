40f54c608462e90336db042d4683f800f4bcebd8
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-07 19:34:53 +0530

    Update mend.groovy

--- vars/mend.groovy
+++ vars/mend.groovy
@@ -1,24 +1,24 @@
-def call() {
-stage('Security Audit') {
-        environment {
-        WS_APIKEY = credentials('mend-api-key')
-        WS_WSS_URL = 'https://saas.whitesourcesoftware.com/agent'
-        WS_PRODUCTNAME = 'HCLCODE'
-        WS_PROJECTNAME = "${applicationName}"
-      }
-      steps {
-        dir(localFolderName) {
-          script {
-            if (IsPackageJsonChanged) {
-              script {
-              echo 'Downloading Mend Unified Agent'
-              sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
-              echo 'Generate Mend Report'
-              sh 'java -jar wss-unified-agent.jar'
-              }
+def call(Map config) {
+    String productName = config.productName ?: 'MyHCLSoftware'
+    String folderName = config.folderName ?: '.'
+    String apiKeyCredentialId = config.apiKeyCredentialId ?: 'mend-api-key'
+    
+    dir(folderName) {
+        withEnv([
+            "WS_PRODUCTNAME=${productName}",
+            "WS_PROJECTNAME=${config.projectName}",
+            "WS_WSS_URL=https://saas.whitesourcesoftware.com/agent"
+        ]) {
+            withCredentials([string(credentialsId: apiKeyCredentialId, variable: 'WS_APIKEY')]) {
+
+                echo 'Running NPM Audit, Job will fail if there are high priority issues'
+                if (config.isPackageJsonChanged) {
+                    echo 'Downloading Mend Unified Agent'
+                    sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
+                    echo 'Generate Mend Report'
+                    sh 'java -jar wss-unified-agent.jar'
+                } 
             }
-          }
         }
-      }
     }
 }
