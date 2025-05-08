c276a00ad9e8f4d9bfe01838359f4aab8b3a2e97
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-08 12:02:32 +0530

    Update mend.groovy

--- vars/mend.groovy
+++ vars/mend.groovy
@@ -2,7 +2,7 @@ def call(Map params) {
     String productName = 'HCLCODE'
     String apiKeyCredentialId = params.apiKeyCredentialId ?: 'mend-api-key'
     
-    dir(localFolderName) {
+    dir(params.localFolderName) {
         withEnv([
             "WS_PRODUCTNAME=${productName}",
             "WS_PROJECTNAME=${params.applicationName}",
@@ -10,7 +10,7 @@ def call(Map params) {
         ]) {
             withCredentials([string(credentialsId: apiKeyCredentialId, variable: 'WS_APIKEY')]) {
                 echo 'Running NPM Audit, Job will fail if there are high priority issues'
-                if (isPackageJsonChanged) {
+                if (params.isPackageJsonChanged) {
                     echo 'Downloading Mend Unified Agent'
                     sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
                     echo 'Generate Mend Report'
