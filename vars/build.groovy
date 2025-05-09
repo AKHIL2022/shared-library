379f1bc38ee3c0c50d40a640c9d3e69d3cd04e94
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-09 17:43:46 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,43 @@
 def call(Map params) {
-    evaluate(new File("${libraryResource('vars/build/' + params.type + '.groovy')}"))
+    validate(params)
+    def script = loadScript(params.type)
+    script.call(params)
+}
+
+private void validate(Map params) {
+    if (!params?.type) {
+        def available = getAvailableScripts()
+        error "Missing 'type' parameter. Available types:\n- ${available.join("\n- ")}"
+    }
+}
+
+private def loadScript(String type) {
+    try {
+        // Convert type "build/mend" to "vars/build/mend.groovy"
+        def scriptPath = "vars/${type}.groovy".toString()
+        
+        if (!fileExists(scriptPath)) {
+            error "Script '${type}' not found at: ${scriptPath}"
+        }
+        
+        return load(scriptPath)
+    } catch(ex) {
+        def available = getAvailableScripts()
+        error "Failed to load '${type}'. Available types:\n- ${available.join("\n- ")}"
+    }
+}
+
+private List<String> getAvailableScripts() {
+    // Get all .groovy files under vars/**/
+    def scripts = []
+    def varsDir = new File("${libraryResource('vars')}")
+    
+    varsDir.eachFileRecurse(groovy.io.FileType.FILES) { file ->
+        if (file.name.endsWith('.groovy')) {
+            def path = file.path.replace("${varsDir.path}/", "").replace(".groovy", "")
+            scripts << path
+        }
+    }
+    
+    return scripts
 }
