7c80e10618456cab41ec0afa3e7f0b7e00ecdec7
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-09 17:52:59 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,43 +1,26 @@
 def call(Map params) {
-    validate(params)
-    def script = loadScript(params.type)
-    script.call(params)
-}
-
-private void validate(Map params) {
-    if (!params?.type) {
-        def available = getAvailableScripts()
-        error "Missing 'type' parameter. Available types:\n- ${available.join("\n- ")}"
-    }
-}
-
-private def loadScript(String type) {
-    try {
-        // Convert type "build/mend" to "vars/build/mend.groovy"
-        def scriptPath = "vars/${type}.groovy".toString()
-        
-        if (!fileExists(scriptPath)) {
-            error "Script '${type}' not found at: ${scriptPath}"
-        }
-        
-        return load(scriptPath)
-    } catch(ex) {
-        def available = getAvailableScripts()
-        error "Failed to load '${type}'. Available types:\n- ${available.join("\n- ")}"
-    }
-}
-
-private List<String> getAvailableScripts() {
-    // Get all .groovy files under vars/**/
-    def scripts = []
-    def varsDir = new File("${libraryResource('vars')}")
+    String productName = 'HCLCODE'
+    String apiKeyCredentialId = params.apiKeyCredentialId ?: 'mend-api-key'
     
-    varsDir.eachFileRecurse(groovy.io.FileType.FILES) { file ->
-        if (file.name.endsWith('.groovy')) {
-            def path = file.path.replace("${varsDir.path}/", "").replace(".groovy", "")
-            scripts << path
+    dir(params.localFolderName) {
+        withEnv([
+            "WS_PRODUCTNAME=${productName}",
+            "WS_PROJECTNAME=${params.projectName}",
+            "WS_WSS_URL=https://saas.whitesourcesoftware.com/agent"
+        ]) {
+            withCredentials([string(credentialsId: apiKeyCredentialId, variable: 'WS_APIKEY')]) {
+                echo 'Running NPM Audit, Job will fail if there are high priority issues'
+                echo "=== Mend Configuration ==="
+                echo "- Application: ${params.projectName}"
+                echo "- Directory: ${params.localFolderName}"
+                echo "- Package.json Changed: ${params.isPackageJsonChanged}"
+                if (params.isPackageJsonChanged) {
+                    echo 'Downloading Mend Unified Agent'
+                    sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
+                    echo 'Generate Mend Report'
+                    sh 'java -jar wss-unified-agent.jar'
+                } 
+            }
         }
     }
-    
-    return scripts
 }
