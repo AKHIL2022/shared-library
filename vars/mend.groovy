3783aa6f58886a28e1a8d668ca27b14544994b74
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-13 15:56:29 +0530

    Update mend.groovy

--- vars/mend.groovy
+++ vars/mend.groovy
@@ -2,25 +2,28 @@ def call(Map params) {
     String productName = 'HCLCODE'
     String apiKeyCredentialId = params.apiKeyCredentialId ?: 'mend-api-key'
     
-    dir(params.folderName) {
+    def runMendScan = {
         withEnv([
             "WS_PRODUCTNAME=${productName}",
-            "WS_PROJECTNAME=${params.projectName}",
+            "WS_PROJECTNAME=${params.applicationName}",
             "WS_WSS_URL=https://saas.whitesourcesoftware.com/agent"
         ]) {
             withCredentials([string(credentialsId: apiKeyCredentialId, variable: 'WS_APIKEY')]) {
                 echo 'Running NPM Audit, Job will fail if there are high priority issues'
-                echo "=== Mend Configuration ==="
-                echo "- Application: ${params.projectName}"
-                echo "- Directory: ${params.folderName}"
-                echo "- Package.json Changed: ${params.IsPackageJsonChanged }"
-                if (params.IsPackageJsonChanged ) {
+                if (params.IsPackageJsonChanged?.toString()?.toBoolean()) {
                     echo 'Downloading Mend Unified Agent'
                     sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
                     echo 'Generate Mend Report'
                     sh 'java -jar wss-unified-agent.jar'
-                } 
+                }
             }
         }
+    }
+    if (params.localFolderName) {
+        dir(params.localFolderName) {
+            runMendScan()
+        }
+    } else {
+        runMendScan()
     }
 }
