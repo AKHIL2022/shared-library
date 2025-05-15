de9d477b2b3b6a9eefd4907bfab8b203b2b5b763
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-15 12:45:48 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,7 +1,7 @@
 def call() {
       script {
         version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
-        s3ObjectName = "${projectName}/${packageName}/${version}.zip"
+        s3ObjectName = "${applicationName}/${packageName}/${version}.zip"
       }
     withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
         s3Upload(
