1f9384355f8afa3a392672dbd7232f4d0c69780a
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-15 12:28:16 +0530

    Create publish.groovy

---
+++ vars/publish.groovy
@@ -0,0 +1,15 @@
+def call() {
+      script {
+        version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
+        s3ObjectName = "${ProjectName}/${packageName}/${version}.zip"
+      }
+    withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
+        s3Upload(
+            pathStyleAccessEnabled: true,
+            payloadSigningEnabled: true,
+            file: bundleFileName,
+            bucket: s3BucketName,
+            path: s3ObjectName
+        )
+    }
+}
