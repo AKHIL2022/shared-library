6c33c410f185f1c03466517096319787f793a663
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-15 16:08:06 +0530

    Create update.groovy

---
+++ vars/update.groovy
@@ -0,0 +1,57 @@
+def call (Map params) {
+      steps {
+        withCredentials([sshUserPrivateKey(credentialsId: gitEnvRepoCredentialsId, keyFileVariable: 'SSH_KEY')]) {
+          sh "GIT_SSH_COMMAND=\"ssh -i \\\"$SSH_KEY\\\"\" git clone --depth=1 --branch ${params.gitEnvDevBranchName} ${params.gitEnvUrl}"
+        }
+        script {
+          gitCommitHash = sh(
+          script: "git log -n 1 --pretty=format:'%H'",
+          returnStdout: true
+        )
+          gitCommitDate = sh(
+          script: "git log -n 1 --pretty=format:'%cI'",
+          returnStdout: true
+        )
+          gitCommitSubject = sh(
+          script: "git log -n 1 --pretty=format:'%s'",
+          returnStdout: true
+        )
+          gitCommitSubject = gitCommitSubject.replace('"', '\\"')
+          gitCommitAuthorName = sh(
+          script: "git log -n 1 --pretty=format:'%aN'",
+          returnStdout: true
+        )
+          gitCommitAuthorEmail = sh(
+          script: "git log -n 1 --pretty=format:'%aE'",
+          returnStdout: true
+        )
+        }
+        dir(params.gitEnvRepoName) {
+          writeFile(
+          file: versionFileName,
+          text: """\
+            # Module: ${params.packageName}
+            locals {
+              ${params.packageName}_commitHash = "${gitCommitHash}"
+              ${params.packageName}_commitDate = "${gitCommitDate}"
+              ${params.packageName}_bucketName = "${params.s3BucketName}"
+              ${params.packageName}_objectName = "${params.s3ObjectName}"
+            }
+            """.stripIndent()
+        )
+          sh "git add ${versionFileName}"
+          sh """\
+          git -c \"user.name=${params.authorName}\" \
+              -c \"user.email=${params.authorEmail}\" \
+              commit -m \"${gitCommitSubject} (${params.packageName})\" \
+              --author=\"${gitCommitAuthorName} <${gitCommitAuthorEmail}>\"
+          """.stripIndent()
+          retry(3) {
+            withCredentials([sshUserPrivateKey(credentialsId: gitEnvRepoCredentialsId, keyFileVariable: 'SSH_KEY')]) {
+              sh "GIT_SSH_COMMAND=\"ssh -i \\\"$SSH_KEY\\\"\" git -c \"user.name=${params.authorName}\" -c \"user.email=${params.authorEmail}\" pull --rebase origin refs/heads/${params.gitEnvDevBranchName}"
+              sh "GIT_SSH_COMMAND=\"ssh -i \\\"$SSH_KEY\\\"\" git push origin HEAD:refs/heads/${params.gitEnvDevBranchName}"
+            }
+          }
+        }
+      }
+}
