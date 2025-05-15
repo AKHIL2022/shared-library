f694c476a94ffc76a4a213bc04f5fbc05282023e
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-15 15:42:17 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,22 +1,18 @@
 def call(Map params) {
-      if (!binding.hasVariable('applicationName'))
-      if (!binding.hasVariable('packageName'))
-      if (!binding.hasVariable('bundleFileName'))
-      if (!binding.hasVariable('s3BucketName'))
-      if (!binding.hasVariable('s3ObjectName'))
-      
       script {
         version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
-        s3ObjectName = "${applicationName}/${packageName}/${version}.zip"
+        s3ObjectName = "${params.applicationName}/${params.packageName}/${version}.zip"
       }
     withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
         echo "Version is: ${version}"  
         echo "S3 Object Path: ${s3ObjectName}"
+        echo "S3 bucketname: ${params.s3BucketName}"
+        echo "S3 Object Path: ${s3ObjectName}"
         s3Upload(
             pathStyleAccessEnabled: true,
             payloadSigningEnabled: true,
-            file: bundleFileName,
-            bucket: s3BucketName,
+            file: params.bundleFileName,
+            bucket: params.s3BucketName,
             path: s3ObjectName
         )
     }
