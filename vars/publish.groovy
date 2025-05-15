1ec21a2d9383da2bb4bba871127f8c393c7f394c
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-15 13:49:32 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -4,6 +4,7 @@ def call(Map params) {
         s3ObjectName = "${params.applicationName}/${params.packageName}/${version}.zip"
       }
     withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
+        echo "s3ObjectName"
         s3Upload(
             pathStyleAccessEnabled: true,
             payloadSigningEnabled: true,
