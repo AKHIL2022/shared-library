252ef34d6cc0101d1351a50b84a34c702fb17ca1
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-26 13:45:09 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,17 +1,28 @@
 def call(Map params) {
-      def s3ObjectName
-      script {
-        def version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
-        s3ObjectName = "${params.applicationName}/${params.packageName}/${version}.zip"
-      }
-    withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
-        s3Upload(
-            pathStyleAccessEnabled: true,
-            payloadSigningEnabled: true,
-            file: params.bundleFileName,
-            bucket: params.s3BucketName,
-            path: s3ObjectName
-        )
-    }
-    return s3ObjectName  
+  def s3ObjectName
+  if (params.hasRelevantChanges || params.force_build) {
+    def uploadToS3 = {
+        script {
+          def version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
+          s3ObjectName = "${params.applicationName}/${params.packageName}/${version}.zip"
+        }
+        withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
+          s3Upload(
+              pathStyleAccessEnabled: true,
+              payloadSigningEnabled: true,
+              file: params.bundleFileName,
+              bucket: params.s3BucketName,
+              path: s3ObjectName
+          )
+        }
+    } return s3ObjectName
+
+    if (params.localFolderName) {
+        dir(params.localFolderName) {
+            uploadToS3()
+        }
+    } else {
+        uploadToS3()
+     }
+  }
 }
