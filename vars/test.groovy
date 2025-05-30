baa8eb33b17f9e3bed903b36e303479cdcafd728
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-30 10:17:00 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,16 +1,7 @@
 def call(Map params) {
     echo "hasRelevantChanges: ${params.HasRelevantChanges}"
     echo "force: ${params.force_build}"
-    if (params.HasRelevantChanges || params.force_build) {
         def buildStep = {
             sh 'npm run build'
         }
-        if (params.localFolder) {
-            dir(params.localFolder) {
-                buildStep()
-            }
-        } else {
-            buildStep()
-        }
-    }
 }
