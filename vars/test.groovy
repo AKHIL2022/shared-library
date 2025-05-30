ed40e7c45094ec9eff8e42a2e0a55c021083e4ca
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-30 10:22:50 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,4 @@
 def call(Map params) {
-    echo "hasRelevantChanges: ${params.HasRelevantChanges}"
-    echo "force: ${params.force_build}"
         def buildStep = {
             sh 'npm run build'
         }
