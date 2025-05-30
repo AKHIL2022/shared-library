fc0e211c02a532c35d39a8c5d1871b54e77eb190
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-30 17:26:00 +0530

    Update checkChanges.groovy

--- vars/checkChanges.groovy
+++ vars/checkChanges.groovy
@@ -41,7 +41,7 @@ def call(String localFolderName) {
         echo "There are no changes that would affect the deployment"
     }
 
-    def returnValues = [isPackageJsonChanged.toString(), hasRelevantChanges.toString()]
+    def returnValues = [isPackageJsonChanged, hasRelevantChanges]
     echo "returnValues: ${returnValues}"
     return returnValues
 }
