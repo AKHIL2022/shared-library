3b98eaac9c9b10227bb285bcb66e6df8c79ccca0
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-30 13:52:57 +0530

    Create checkChanges.groovy

---
+++ vars/checkChanges.groovy
@@ -0,0 +1,44 @@
+def call(String localFolderName) {
+    def changes = []
+    def returnValues = []
+    def build = currentBuild
+    def relevant
+    def isPackageJsonChanged
+    def hasRelevantChanges
+    while (build != null && build.result != 'SUCCESS') {
+        for (changeLog in build.changeSets) {
+            for (entry in changeLog.items) {
+                for (file in entry.affectedFiles) {
+                    changes += file.path
+                }
+            }
+        }
+        build = build.previousBuild
+        if (!build) {
+            changes = ["${localFolderName}/*"]
+        }
+    }
+    changes.unique().sort()
+    echo "Changed since last successful build: ${changes.isEmpty() ? 'none' : changes.join(', \n')}"
+    relevant = changes.findAll { element ->
+        // Include changes to our localFolderPath
+        element ==~ /\Q$localFolderName\E\/.*/
+    }
+    relevant = relevant.findAll { element ->
+        // Ignore changes to *.test.js files
+        !(element ==~ /.*\.test\.js/)
+    }
+    isPackageJsonChanged = relevant.any { element ->
+        element ==~ /.*package-lock\.json/
+    }
+    hasRelevantChanges = !relevant.isEmpty()
+    if (hasRelevantChanges) {
+        echo "There are changes that affect the deployment: ${Relevant.join(', ')}"
+    } else {
+        echo 'There are no changes that would affect the deployment'
+    }
+    returnValues.addAll([isPackageJsonChanged, hasRelevantChanges])
+    return returnValues
+    echo "isPackageJsonChanged: ${isPackageJsonChanged}"
+    echo "hasRelevantChanges: ${hasRelevantChanges}"
+}
