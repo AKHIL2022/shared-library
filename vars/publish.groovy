abb55d9755da6aa9eebff691f13b186d405905ef
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-05-30 12:20:41 +0530

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,12 +1,12 @@
-def call(String applicationName, String componentName, String bundleFileName, String s3BucketName ) {
+def call(String applicationName, String packagename, String bundleFileName, String s3BucketName ) {
   echo "applicationName: ${applicationName}"
-  echo "componentName: ${componentName}"
+  echo "packagename: ${packagename}"
   echo "bundleFileName: ${bundleFileName}"
   echo "s3BucketName: ${s3BucketName}"
   def s3ObjectName
         script {
           def version = sh(script: "echo -n v\$(date +%Y%m%d-%H%M%S)", returnStdout: true)
-          s3ObjectName = "${applicationName}/${componentName}/${version}.zip"
+          s3ObjectName = "${applicationName}/${packagename}/${version}.zip"
         }
         withAWS(region: 'us-east-1', credentials: 'aws-deployment-backend') {
           s3Upload(
