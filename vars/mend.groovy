e8bf43318f0eede8f5ed35bff04d21609f96bc20
AKHIL2022 <63500730+AKHIL2022@users.noreply.github.com>
2025-08-06 17:28:08 +0530

    Update mend.groovy

--- vars/mend.groovy
+++ vars/mend.groovy
@@ -1,4 +1,6 @@
-   def call(String projectName, boolean isPackageJsonChanged, boolean continueOnAuditFail) {
+   def call(String projectName, isPackageJsonChanged, continueOnAuditFail) {
+    boolean isPackageJsonChangedBool = isPackageJsonChanged.toBoolean()
+    boolean continueOnAuditFailBool = continueOnAuditFail.toBoolean()
     String productName = 'HCLCODE'
     String apiKeyCredentialId = params.apiKeyCredentialId ?: 'mend-api-key'
         withEnv([
@@ -11,18 +13,18 @@
                 def highVul = sh(script: 'npm audit --audit-level=high', returnStatus: true)
                 if (criticalVul != 0) {
                     echo "npm audit found issues (exit code: ${criticalVul})"
-                    if (continueOnAuditFail == false) {
+                    if (continueOnAuditFailBool == false) {
                         echo "${continueOnAuditFail}"
                         error('Failing pipeline due to audit errors.')
                     } else {
-                        echo "${continueOnAuditFail}"
+                        echo "${continueOnAuditFailBool}"
                         unstable("Proceeding despite critical audit issues.")
                     }
                 }
                 if (highVul != 0) {
                     unstable('Proceeding despite audit issues.')
                 }
-                if (isPackageJsonChanged) {
+                if (isPackageJsonChangedBool) {
                     echo 'Downloading Mend Unified Agent'
                     sh 'curl -LJO https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar'
                     echo 'Generate Mend Report'
